9e395ee243469bdfae284ac46dff3838c826d735
Kirandeep <kirandeep1195@gmail.com>
2023-07-16 18:58:39 -0400

    made routes and server file

---
+++ Develop/routes/index.js
@@ -0,0 +1,12 @@
+
+// Importing our files containing our routes
+const express = require('express')
+const notesRouter = require('./notes');
+
+// Create and instance of express so we can apply the middleware and routing
+ const app = express();
+// http://localhost:3001/api/notes
+ app.use("/notes",notesRouter);
+
+ module.exports = app;
+
---
+++ Develop/routes/notes.js
@@ -0,0 +1,60 @@
+const notes = require('express').Router();
+const uuid = require('../helpers/uuid');
+const fs = require('fs');
+
+
+//GET /notes should return the notes.html file.
+
+
+
+
+
+// http://localhost:3001/api/notes/
+//GET /api/notes should read the db.json file and return all saved notes as JSON.
+notes.get('/', (req, res) => {
+  console.info(`${req.method} note maker`);
+  fs.readFile('./db/db.json', 'utf8', function (error, data) { res.json(JSON.parse(data)) });
+})
+
+//POST /api/notes should receive a new note to save on the request body, add it to the db.json file,
+// and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved .
+
+notes.post('/', (req, res) => {
+  console.log(`${req.method} request received`)
+  const { title, text } = req.body;
+
+
+  const newNote = {
+    title,
+    text,
+    id: uuid(),
+
+  };
+
+  fs.readFile('./db/db.json', 'utf8', function (error, data) {
+    const newData = JSON.parse(data)
+    newData.push(newNote)
+
+
+
+    // Convert the data to a string so we can save it
+    const noteString = JSON.stringify(newData)
+
+    // Write the string to a file
+    fs.writeFile(`./db/db.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        :
+
+
+        res.json(newData)
+    );
+  })
+})
+
+
+
+
+//GET * should return the index.html file.
+
+module.exports = notes;
